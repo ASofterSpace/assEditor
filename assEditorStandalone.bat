a5f03b02fa1cac1190f70e8dcdf5ba8ed339bdbc
Tom Moya Schiller <moya@asofterspace.com>
2020-05-11 23:15:30 +0200

    Adjust standalone startup scripts for renaming of Main

--- assEditorStandalone.bat
+++ assEditorStandalone.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start "A Softer Space Editor" javaw -classpath "%~dp0\bin" -Xms16m -Xmx1024m com.asofterspace.assEditor.Main --standalone
+start "A Softer Space Editor" javaw -classpath "%~dp0\bin" -Xms16m -Xmx1024m com.asofterspace.assEditor.AssEditor --standalone
